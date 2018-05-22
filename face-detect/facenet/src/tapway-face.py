ac4f597982388a15cd329c8085e93b0bbdead920
oka96 <keeaun_96@yahoo.com>
2018-05-22 13:15:12 +0800

    add register name feature

--- face-detect/facenet/src/tapway-face.py
+++ face-detect/facenet/src/tapway-face.py
@@ -32,14 +32,10 @@ threshold = [ 0.6, 0.7, 0.9 ]  # three steps's threshold
 factor = 0.709 # scale factor
 
 def saveImage(saveImgPath,cropFace,numFace):
-	if os.path.isdir(saveImgPath):
-		cv2.imwrite('{}/face_{}.png'.format(saveImgPath,numFace),cropFace)      
-	else:
-		print('{} path does not exists.'.format(saveImgPath))
-
-def doRecognizePerson(faceNames,fid):
-	time.sleep(2)
-	# faceNames[fid] = "Person "+str(fid)
+	if not os.path.isdir(saveImgPath):
+		os.mkdir(saveImgPath)
+		print('Create new {} path'.format(saveImgPath))
+	cv2.imwrite('{}/face_{}.png'.format(saveImgPath,numFace),cropFace)      
 
 ### create image that min 80*80 pixel for aws rekognition
 def cropFaceAWS(img,bb,crop_factor):
@@ -56,7 +52,7 @@ def cropFaceAWS(img,bb,crop_factor):
 	crop_y1 = int(y1-crop_factor*h)
 	crop_y2 = int(y2+crop_factor*h)
 	crop_x1 = int(x1-crop_factor*w)
-	crop_x2 = int(x2+crop_factor*w) 
+	crop_x2 = int(x2+crop_factor*w)
 
 	if crop_y1 < 0:
 		crop_y1 = 0
@@ -93,13 +89,24 @@ def cropFaceAWS(img,bb,crop_factor):
 class GUI(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		root = tk.Tk.__init__(self, *args, **kwargs)
-		self.file = 0
-		# self.file = "rtsp://admin:admin@192.168.0.8/play1.sdp"
 
-		self.detect = FaceDetect()
+		self.config = self.readConfigFile()
+
+		self.file = self.config['video']
+		self.saveImgPath = self.config['imagePath']
+
+		self.frame_interval = 10
+		self.frame_count = 0
+
+		self.num_face = 0
+		self.currentFaceID = 0
+
+		self.tracker = track.Tracker()
+		self.crop_factor = 0.25
+
 		self.winfo_toplevel().resizable(width=False,height=False)
-
 		self.winfo_toplevel().title('Tapway Face System')
+
 		self.videoFrame = tk.Frame(root,width=600,height=600,borderwidth=0)
 		self.videoFrame.pack(side=tk.LEFT)
 		
@@ -113,40 +120,69 @@ class GUI(tk.Tk):
 		self.imageList = []
 
 		### read single frame to setup imageList
+		'''
 		_,frame = self.camera.read()
 
 		height,_,_ = frame.shape
 		row = math.ceil(height/120)
 
 		self.addImageList(row*2)
+		'''
+
+		self.addImageList(2)
+
+		self.faceNamesList = {}
+
+	def readConfigFile(self):
+		with open('config.json') as configuration:
+			conf = json.load(configuration)
+		return conf
+
+	def resizeImage(self,sizeX,sizeY,img):
+		height,width,_ = img.shape
+		scaleY = sizeY/height
+		scaleX = sizeX/width
+		resizeImg = cv2.resize(img,(0,0),fx=scaleX,fy=scaleY)
+		return cv2.cvtColor(resizeImg,cv2.COLOR_BGR2RGBA)
 
 	def addImageList(self,row):
 		photo = cv2.imread('gui/icon.jpg')
-
-		height,width,_ = photo.shape
-		scaleY = 100.0/height
-		scaleX = 100.0/width
-		resizeImg = cv2.resize(photo,(0,0),fx=scaleX,fy=scaleY)
-		cv2image = cv2.cvtColor(resizeImg,cv2.COLOR_BGR2RGBA)
+		cv2image = self.resizeImage(100,100,photo)
 
 		img = Image.fromarray(cv2image)
 		imgtk = ImageTk.PhotoImage(image=img)
 
 		for i in range(row):
-			lb = tk.Label(self.frame.interior,image=imgtk)
-			lb.imgtk = imgtk
-			if i%2 == 0:
-				lb.grid(row=int(i/2),column=i%2,padx=(10,5),pady=10)
-			else:
-				lb.grid(row=int(i/2),column=i%2,padx=(5,10),pady=10)
+			imgLabel = tk.ttk.Button(self.frame.interior,image=imgtk,state='disable')
+			imgLabel.imgtk = imgtk
+			self.imageList.append(imgLabel)
+		self.gridResetLayout()
 
-			self.imageList.append(lb)
+	def addFaceToImageList(self,fid,cropface,awsID):
+		cv2image = self.resizeImage(100,100,cropface)
+
+		img = Image.fromarray(cv2image)
+		imgtk = ImageTk.PhotoImage(image=img)
+
+		imgLabel = tk.ttk.Button(self.frame.interior,image=imgtk,command=lambda:self.popup(fid,imgtk,awsID))
+		imgLabel.imgtk = imgtk
+		self.imageList.append(imgLabel)
+		self.gridResetLayout()
+
+	def gridResetLayout(self):
+		### reverse image list for make the latest image in the top of list for GUI view
+		for index,item in enumerate(reversed(self.imageList)):
+			item.grid(row=0,column=0)
+			if index%2==0:
+				item.grid(row=int(index/2),column=index%2,padx=(10,5),pady=10)
+			else:
+				item.grid(row=int(index/2),column=index%2,padx=(5,10),pady=10)
 
 	def show_frame(self):
 		flag, frame = self.camera.read()
 		if flag == True:
 			### facenet accept img shape (?,?,3)
-			self.detect.detectFace(frame,self.imageList,self.frame.interior)
+			self.detectFace(frame)
 			### convert img shape to (?,?,4)
 			cv2image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
 
@@ -158,43 +194,37 @@ class GUI(tk.Tk):
 			print('No frame come in')
 		self.videoLabel.after(10,self.show_frame)
 
-class FaceDetect:
-	def __init__(self,*args,**kwargs):
-		self.saveImgPath = 'face_img'
-
-		self.frame_interval = 10
-		self.frame_count = 0
-
-		self.num_face = 0
-		self.currentFaceID = 0
-
-		self.tracker = track.Tracker()
-		self.crop_factor = 0.25
-
-	def AWSRekognition(self,enc,fid):
+	def AWSRekognition(self,enc,cropface,fid):
 		try:
-			result  = aws.search_faces(enc)
-			if len(result['FaceMatches']) == 0:
-				print('New face add to collection')
+
+			res  = aws.search_faces(enc)
+			if len(res['FaceMatches']) == 0:
 				res = aws.index_faces(enc)
-				print('Face ID:',res['FaceRecords'][0]['Face']['FaceId'])
-				self.tracker.faceNames[fid] = "New Face ID: "+res['FaceRecords'][0]['Face']['FaceId']
+				awsID = res['FaceRecords'][0]['Face']['FaceId']
+
+				self.tracker.faceID[fid] = awsID
+				self.addFaceToImageList(fid,cropface,awsID)
+
+				print('New Face ID:',awsID)
+
 			else:
-				# add aws id
-				self.tracker.faceNames[fid] = "Match ID: "+result['FaceMatches'][0]['Face']['FaceId']
-				print('Match Face ID {}'.format(result['FaceMatches'][0]['Face']['FaceId']))
+				awsID = res['FaceMatches'][0]['Face']['FaceId']
+
+				self.tracker.faceID[fid] = awsID
+				self.addFaceToImageList(fid,cropface,awsID)
+
+				print('Match Face ID {}'.format(awsID))
 
 		except Exception as e:
-			# using except statement become index_faces from aws rekognition will have error if does not detect any face
+			# aws rekognition will have error if does not detect any face
 			if type(e).__name__ == 'InvalidParameterException':
-				### delete the face, if aws cant detect it as a face
+				### delete the face, since aws cant detect it as a face
 				self.tracker.appendDeleteFid(fid)
 			else:
 				print(e)
 			pass
 
 	def drawTrackedFace(self,imgDisplay):
-		rectangleColor = (0,165,255)
 
 		for fid in self.tracker.faceTrackers.keys():
 			tracked_position = self.tracker.faceTrackers[fid].get_position()
@@ -203,30 +233,62 @@ class FaceDetect:
 			t_w = int(tracked_position.width())
 			t_h = int(tracked_position.height())
 
+			text = '...Detecting...'
+			rectColor = (0,165,255)
+
+			if fid in self.tracker.faceID.keys():
+
+				awsID = self.tracker.faceID[fid]
+
+				if awsID in self.faceNamesList.keys():
+					text = self.faceNamesList[awsID]
+					rectColor = (255,0,0)
+				else:
+					text = awsID
+					rectColor = (0,0,255)
+
+			textSize = cv2.getTextSize(text,cv2.FONT_HERSHEY_SIMPLEX,0.5,2)[0]
+			textX = int(t_x+t_w/2-(textSize[0])/2)
+			textY = int(t_y)
+			textLoc = (textX,textY)
+
 			cv2.rectangle(imgDisplay, (t_x, t_y),
 									(t_x + t_w , t_y + t_h),
-									rectangleColor ,2)
+									rectColor ,2)
 
-			if fid in self.tracker.faceNames.keys():
-				# cv2.putText(imgDisplay, self.tracker.faceNames[fid] , 
-				# 			(int(t_x + t_w/2), int(t_y)), 
-				# 			cv2.FONT_HERSHEY_SIMPLEX,
-				# 			0.5, (255, 255, 255), 2)
-				text = self.tracker.faceNames[fid]
-				textsize = cv2.getTextSize(text,cv2.FONT_HERSHEY_SIMPLEX,0.5,2)[0]
-				textX = (textsize[0])/2
-				# textY = (t_w+textsize[1])/2
-				cv2.putText(imgDisplay, self.tracker.faceNames[fid] , 
-							(int(t_x+t_w/2-textX), int(t_y)), 
-							cv2.FONT_HERSHEY_SIMPLEX,
-							0.5, (255, 255, 255), 2)
-			else:
-				cv2.putText(imgDisplay, "Detecting..." , 
-							(int(t_x + t_w/2), int(t_y)), 
-							cv2.FONT_HERSHEY_SIMPLEX,
-							0.5, (255, 255, 255), 2)
+			cv2.putText(imgDisplay, text, textLoc, 
+						cv2.FONT_HERSHEY_SIMPLEX,
+						0.5, (255, 255, 255), 2)
 
-	def detectFace(self,imgDisplay,imageList,interior):
+	def popup(self,fid,imgtk,awsID):
+		print(awsID)
+		win = tk.Toplevel()
+		win.wm_title("Register Face")
+
+		imgLabel = tk.Label(win,image=imgtk)
+		imgLabel.imgtk = imgtk
+		imgLabel.pack(fill=tk.X,expand=True)
+
+		faceid = tk.Label(win,text='Face Id: {0}'.format(awsID))
+		faceid.pack(fill=tk.X)
+
+		nameFrame = tk.ttk.Frame(win)
+		nameFrame.pack(fill=tk.X,pady=5)
+
+		nameLabel = tk.ttk.Label(nameFrame,text='Name: ')
+		nameLabel.pack(side=tk.LEFT,padx=5)
+
+		nameEntry = tk.ttk.Entry(nameFrame)
+		nameEntry.pack(side=tk.RIGHT,padx=5)
+
+		submit = tk.ttk.Button(win,text='Submit',width=10,command=lambda:self.submit(awsID,nameEntry.get(),win))
+		submit.pack(pady=5)
+
+	def submit(self,awsID,name,win):
+		self.faceNamesList[awsID] = name
+		win.destroy();
+
+	def detectFace(self,imgDisplay):
 		### Avoid use imgDisplay = frame
 		frame = imgDisplay.copy()
 
@@ -236,7 +298,6 @@ class FaceDetect:
 		if (self.frame_count%self.frame_interval) == 0:
 			bounding_boxes,_ = align.detect_face.detect_face(imgDisplay, minsize, pnet, rnet, onet, threshold, factor)
 
-			#   for each box
 			for (x1, y1, x2, y2, acc) in bounding_boxes:
 
 				w = int(x2-x1)
@@ -244,64 +305,25 @@ class FaceDetect:
 				x = int(x1)
 				y = int(y1)
 
-				matchedFid = self.tracker.getMatchId(x,y,w,h)
+				matchedFid = self.tracker.getMatchId(imgDisplay,x,y,w,h)
 
 				if matchedFid is None:
 					self.currentFaceID += 1
 					self.num_face += 1
 
 					self.tracker.createTrack(imgDisplay,x,y,w,h,self.currentFaceID)
-					t = threading.Thread(target=doRecognizePerson,args=(self.tracker.faceNames,self.currentFaceID))
-					t.start()
 
 					cropface = cropFaceAWS(frame,(x1,x2,y1,y2),self.crop_factor)
 					saveImage(self.saveImgPath,cropface,self.num_face)
 						
 					enc = cv2.imencode('.png',cropface)[1].tostring()
-					t2 = threading.Thread(target=self.AWSRekognition,args=([enc,self.currentFaceID]))
+					t2 = threading.Thread(target=self.AWSRekognition,args=([enc,cropface,self.currentFaceID]))
 					t2.start()
-
-					### Make sure image list will have one row more than the num face
-					if len(imageList) < self.num_face+2:
-						photo = cv2.imread('gui/icon.jpg')
-
-						height,width,_ = photo.shape
-						scaleY = 100.0/height
-						scaleX = 100.0/width
-						resizeImg = cv2.resize(photo,(0,0),fx=scaleX,fy=scaleY)
-						cv2image = cv2.cvtColor(resizeImg,cv2.COLOR_BGR2RGBA)
-
-						img = Image.fromarray(cv2image)
-						imgtk = ImageTk.PhotoImage(image=img)
-
-						for i in range(2):
-							r = self.num_face+int(i/2)
-							c = i%2
-							lb = tk.Label(interior,image=imgtk)
-							lb.imgtk = imgtk
-							if i%2 == 0:
-								lb.grid(row=r,column=c,padx=(10,5),pady=10)
-							else:
-								lb.grid(row=r,column=c,padx=(5,10),pady=10)
-
-							imageList.append(lb)
-
-					height,width,_ = cropface.shape
-					scaleY = 100.0/height
-					scaleX = 100.0/width
-					resizeImg = cv2.resize(cropface,(0,0),fx=scaleX,fy=scaleY)
-					cv2image = cv2.cvtColor(resizeImg,cv2.COLOR_BGR2RGBA)
-
-					img = Image.fromarray(cv2image)
-					imgtk = ImageTk.PhotoImage(image=img)
-
-					index = self.num_face-1
-					imageList[index].imgtk = imgtk
-					imageList[index].configure(image=imgtk)
 		
 		self.drawTrackedFace(imgDisplay)
 
 if __name__ == '__main__':
+
 	with tf.Graph().as_default():
 		gpu_options = tf.GPUOptions(
 			per_process_gpu_memory_fraction=gpu_memory_fraction)
