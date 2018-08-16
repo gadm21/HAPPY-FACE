1bfdd528790b57538ff2bfe3472e6c62911a8ee5
zhiqin <zhiqin1998@hotmail.com>
2018-08-16 17:51:32 +0800

    change the vehicle runner path

--- vehicle-recognition/server_settings.py
+++ vehicle-recognition/server_settings.py
@@ -1,2 +1,2 @@
 # Must be the fullpath to where the frames are saved
-SAVE_DIR = "/home/nvidia/tapway-ai/vehicle-recognition/frames/"
+SAVE_DIR = "/home/nvidia/Desktop/tapway-ai/vehicle-recognition/frames/"
